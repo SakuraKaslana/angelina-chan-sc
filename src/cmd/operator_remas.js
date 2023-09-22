0f7a79bbdcb80a3114c3141ee66c6a6fabf2b6d2
Nguyễn Phạm Gia Bảo <giabao.nguyenpham@e.email>
2023-09-22 10:33:53 +0700

    operator.js remas - part 3: yay we have a reply

--- src/cmd/operator_remas.js
+++ src/cmd/operator_remas.js
@@ -23,13 +23,24 @@ if no need, we can just do an `op.name` reply directly.
 2023-09-21 
 - 20:42 +07: now I just relized that a better way to do this is just to use json data as a lookup table... I need a break. -giabao06
 - 21:08 +07: well now I thought of just making every operator a json object then send it... what's wrong with me -giabao06
+- 21:28 +07: never mind, a lookup table would still be needed :skull: (for now)
 */
 
 const { MessageFlags } = require ("discord.js");
 const command = require("../structures/Command.js");
 var oplist = require ('../data/op_remas.json')
 
-module.exports = new Command({
+//add new operators here if there is an op_remas.json update
+
+const list6 = ["siege", "angelina", "skalter", "w", "ceobe", "dusk", "chen", "blaze", "kaltsit", "chalter", "passenger", "skadi", "mudrock", "shining", "nightingale"];
+const list5 = [];
+const list4 = ["ambriel"];
+const list3 = ["fang", "vanilla", "hibiscus", "ansel", "lava", "plume", "steward", "kroos", "adnachiel", "orchid", "melantha", "beagle", "spot", "cardi", "catapult", "midnight", "popukar"];
+const list2 = ["yato", "durin", "12f"];
+const list1 = ["lancet", "castle-3", "thermal-ex"]
+
+
+module.exports = new command({
         name: "operator_remas",
         description: "operator.js remas version",
         alias: ["opr"],
@@ -37,17 +48,24 @@ module.exports = new Command({
             if (message.author.bot) return;
             var opname = args.slice(1).join(" ").toLowerCase();
             //commonly used names must be defined to our names
-            if (opname === "skalter") {var opname = "skadialt"};
-            if (opname === "chalter" || opname === "chen the holung day") {var opname = "chenalt"};
-            if (opname === "thermal-ex") {var opname = "thrm-ex"}; //no entry, wtf sakura
+            if (opname === "chen the holung day") {var opname = "chalter"};
             if (opname === "eyja") {var opname = "eyjafjalla"};  //no entry for this either
             if (opname === "exu") {var opname = "exusiai"}; //no entry for this too...
             if (opname === "sa") {var opname = "silverash"};
-
-            //for the part after this:
-            //1. find op name in oplist.chr_6st, if not found then 5, 4, ... then robots
-            //2. if found, print op name, then break
-            //3. if not found, print the not found text
-
+            if (opname === "kal'tsit") var opname = "kaltsit";
+            var found=0;
+            for (let i=0; i<=list6.length; i++) {
+                if (opname === list6[i]) {message.reply(oplist.chr_6st[opname]); found=1; break}}
+            for (let i=0; i<=list5.length; i++) {
+                if (opname === list5[i]) {message.reply(oplist.chr_5st[opname]); found=1; break}}
+            for (let i=0; i<=list4.length; i++) {
+                if (opname === list4[i]) {message.reply(oplist.chr_4st[opname]); found=1; break}}
+            for (let i=0; i<=list3.length; i++) {
+                if (opname === list3[i]) {message.reply(oplist.chr_3st[opname]); found=1; break}}
+            for (let i=0; i<=list2.length; i++) {
+                if (opname === list2[i]) {message.reply(oplist.chr_2st[opname]); found=1; break}}
+            for (let i=0; i<=list1.length; i++) {
+                if (opname === list1[i]) {message.reply(oplist.chr_robots[opname]); found=1; break}}
+            if (found === 0 ) message.reply('Operator currently not in database. Currently available operators: (this part isnt made yet)')
         }
 })
